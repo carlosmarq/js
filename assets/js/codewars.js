f30ed9a5cb047ab90258ffd9787e5ec14be71966
Carlos <carlosmarq@gmail.com>
2017-08-08 22:01:54 -0500

    js

---
+++ assets/js/codewars.js
@@ -0,0 +1,14 @@
+n=128;
+texto=n.toString(2);
+console.log(texto);
+unos=0;
+console.log(texto.length);
+console.log(texto.charAt(0));
+console.log(texto.charAt(1));
+
+
+
+      if(charAt(1) === "1"
+      unos=unos+1;
+      }else{};
+      
