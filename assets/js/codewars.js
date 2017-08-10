7a0d12227f899f27f28cd14f39241697acaadfe1
Carlos <carlosmarq@gmail.com>
2017-08-09 22:01:00 -0500

    js+css+html

--- assets/js/codewars.js
+++ assets/js/codewars.js
@@ -1,14 +1,66 @@
-n=128;
+/*n=128;
 texto=n.toString(2);
 console.log(texto);
-unos=0;
+var unos=0;
 console.log(texto.length);
 console.log(texto.charAt(0));
 console.log(texto.charAt(1));
+console.log(texto.charAt(2));
+console.log(texto.charAt(3));
+console.log(texto.charAt(4));
+console.log(texto.charAt(5));
+console.log(texto.charAt(6));
+console.log(texto.charAt(7));
 
 
 
-      if(charAt(1) === "1"
-      unos=unos+1;
-      }else{};
-      
+    if(texto.charAt(0) === "1"){
+    unos = unos + 1;
+    console.log(unos);
+    }else{
+    console.log("No es un 0");
+        }
+
+
+    for (i = -1; i < texto.length; i++) {
+        console.log(texto.charAt(i));
+          if(texto.charAt(0) === "1"){
+            unos = unos + 1;
+            console.log(unos);
+        }else{
+            console.log("No es un 0");
+                }
+              }
+*/
+
+//En forma de funion:
+/*
+var countBits = function(n) {
+  var texto=n.toString(2);
+  console.log(texto);
+  var unos= 0;
+  for (i = 0; i < texto.length; i++) {
+    console.log(texto.charAt(i));
+    if(texto.charAt(i) === "1"){
+      unos = unos + 1;
+      console.log("Pos",i,"era 1");
+    }else{
+      console.log("Pos",i,"era 0");
+    }
+  }
+  console.log(unos);
+};
+*/
+//Pa Codewars:
+
+var countBits = function(n) {
+  var texto=n.toString(2);
+  var unos= 0;
+  for (i = 0; i < texto.length; i++) {
+    if(texto.charAt(i) === "1"){
+      unos = unos + 1;
+    }else{
+    }
+  }
+  return (unos);
+};
---
+++ assets/js/codewars0.js
@@ -0,0 +1,47 @@
+var biginput= [[45, 12],[55,21],[19, -2],[104, 20]]
+var i=biginput.length;
+console.log(i);
+
+
+
+
+
+for (i = 0; i < biginput.length; i++) {
+
+  var input = biginput.indexOf(i);
+  console.log(i);
+  console.log(input);
+
+  /*
+  var age = input.slice(0,1);
+  var handicap = input.slice(1);
+  console.log(age);
+  console.log(handicap);
+
+  if((age>=55)&&(handicap>7) ){
+    var out="Senior";
+    }else{
+    var out="Open";
+    }
+  console.log(out);
+*/
+  }
+
+
+
+/*
+var input=[69, -9];
+var age = input.slice(0,1);
+var handicap = input.slice(1);
+
+console.log(age);
+console.log(handicap);
+
+if((age>=55)&&(handicap>7) ){
+  var out="Senior";
+  }else{
+  var out="Open";
+  }
+console.log(out);
+
+*/
